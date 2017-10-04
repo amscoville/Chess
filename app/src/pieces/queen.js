2c6bd17af50081e44859808de3297480d6224687
Amy Scoville <amyscoville@Amys-MacBook-Air.local>
2017-10-04 14:59:26 -0600

    Working Queen

--- app/src/pieces/queen.js
+++ app/src/pieces/queen.js
@@ -1,4 +1,6 @@
 import Piece from './piece';
+import Rook from '../pieces/rook';
+import Bishop from '../pieces/bishop';
 // import BoardState from '../board/boardState';
 
 
@@ -6,5 +8,16 @@ export default class Queen extends Piece {
 	constructor(color, row, col) {
 		const img = color === 'white' ? '2655' : '265B';
 		super(color, row, col, img);
+		this.targets = [];
+		this.rook = new Rook();
+		this.bishop = new Bishop();
+	}
+
+	getTargets(row, col) {
+		this.targets = [];
+		const rookTargs = this.rook.getTargets(row, col);
+		const bishopTargs = this.bishop.getTargets(row, col);
+		this.targets.push(...rookTargs, ...bishopTargs);
+		return this.targets;
 	}
 }
