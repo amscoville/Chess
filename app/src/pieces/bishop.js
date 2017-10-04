d03e598571e649e75c590ed205b332b89603b1da
Amy Scoville <amyscoville@Amys-MacBook-Air.local>
2017-10-04 14:04:59 -0600

    Rook piece moves correctly

--- app/src/pieces/bishop.js
+++ app/src/pieces/bishop.js
@@ -6,5 +6,83 @@ export default class Bishop extends Piece {
 	constructor(color, row, col) {
 		const img = color === 'white' ? '2657' : '265D';
 		super(color, row, col, img);
+		this.targets = [];
+	}
+
+	
+	getTargets(row, col) { // check if it's the king
+		this.targets = [];
+		const origCol = Number(col);
+		const origRow = Number(row);
+		const northEastTargs = this.findNorthEastTargs(origRow, origCol);
+		const southWestTargs = this.findSouthWestTargs(origRow, origCol);
+		const northWestTargs = this.findNorthWestTargs(origRow, origCol);
+		const southEastTargs = this.findSouthEastTargs(origRow, origCol);
+		this.targets.push(...northEastTargs, ...southWestTargs, ...northWestTargs, ...southEastTargs);
+		return this.targets;
+	}
+
+	findNorthEastTargs(row, col) {
+		let newRow = row;
+		for (let i = col + 1; i < 8; i++){
+			newRow++;
+			if (BoardState.describeSquare(newRow, i) === 'empty') {
+				arr.push(...this.spaceToArr(newRow, i));
+			} else if (BoardState.describeSquare(newRow, i) === 'enemy') {
+				arr.push(...this.spaceToArr(newRow, i));
+				break;
+			} else if (BoardState.describeSquare(newRow, i) === 'ally') {
+				break;
+			}
+		}
+		return arr;
+	}
+
+	findSouthWestTargs(row, col) {
+		let newRow = row;
+		for (let i = col - 1; i >= 0; i--){
+			newRow--;
+			if (BoardState.describeSquare(newRow, i) === 'empty') {
+				arr.push(...this.spaceToArr(newRow, i));
+			} else if (BoardState.describeSquare(newRow, i) === 'enemy') {
+				arr.push(...this.spaceToArr(newRow, i));
+				break;
+			} else if (BoardState.describeSquare(newRow, i) === 'ally') {
+				break;
+			}
+		}
+		return arr;
+	}
+
+	findNorthWestTargs(row, col) {
+		let newRow = row;
+		for (let i = col - 1; i >= 0; i--){
+			newRow++;
+			if (BoardState.describeSquare(newRow, i) === 'empty') {
+				arr.push(...this.spaceToArr(newRow, i));
+			} else if (BoardState.describeSquare(newRow, i) === 'enemy') {
+				arr.push(...this.spaceToArr(newRow, i));
+				break;
+			} else if (BoardState.describeSquare(newRow, i) === 'ally') {
+				break;
+			}
+		}
+		return arr;
+	}
+
+	findSouthEastTargs(row, col) {
+		let newRow = row;
+		for (let i = col + 1; i < 8; i++){
+			newRow--;
+			if (BoardState.describeSquare(newRow, i) === 'empty') {
+				arr.push(...this.spaceToArr(newRow, i));
+			} else if (BoardState.describeSquare(newRow, i) === 'enemy') {
+				arr.push(...this.spaceToArr(newRow, i));
+				break;
+			} else if (BoardState.describeSquare(newRow, i) === 'ally') {
+				break;
+			}
+		}
+		return arr;
 	}
 }
