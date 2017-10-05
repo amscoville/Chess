5f5b6d8a4b6e51d04ba0fc2a4e68043aa60d5e42
Amy Scoville <amyscoville@Amys-MacBook-Air.local>
2017-10-05 16:08:17 -0600

    Begin Pawn movement

--- app/src/script.js
+++ app/src/script.js
@@ -41,9 +41,9 @@ function createBoard() {
 
 function displayPieces() {
 	for (let i = 0; i < 8; i++) {
-		if (i === 1) {
-			BoardState.state[i] = [];
-		}
+		// if (i === 1) {
+		// 	BoardState.state[i] = [];
+		// }
 		for (let j = 0; j < BoardState.state[i].length; j++) {
 			if (BoardState.state[i][j]) {
 				$(`#${BoardState.state[i][j].row}${BoardState.state[i][j].col}`).html(BoardState.state[i][j].img);
